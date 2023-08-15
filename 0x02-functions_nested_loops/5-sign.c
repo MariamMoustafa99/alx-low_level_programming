c715cc1d712451224c82750f8ee0f1efce3b5177
MariamMoustafa99 <mariam.moustafa57@gmail.com>
2023-08-15 06:30:04 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: number to be checked
- * Return 1 if greater than 0 or 0 if 0 or -1 if less than 0
+ * Return: 1 if greater than 0 or 0 if 0 or -1 if less than 0
  */
 
 int print_sign(int n)
