f6829fc22579a614c19b460b9d626046bda3b57e
MariamMoustafa99 <mariam.moustafa57@gmail.com>
2023-08-21 12:17:56 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
