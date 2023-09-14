fd86e484dce1cf6955fc0f9f67b934f53be62ea3
Mariam <mariam.moustafa57@gmail.com>
2023-09-14 02:11:17 -0700

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,7 +23,10 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(98);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(98);
+		printf("Error\n"), exit(99);
+
+	if (b == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
 
 	printf("%d\n", op_func(a, b));
 	return (0);
