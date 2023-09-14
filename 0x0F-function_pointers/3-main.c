58eeba61b78fad5fb82afb5c37ac9990a74bc302
Mariam <mariam.moustafa57@gmail.com>
2023-09-14 02:16:44 -0700

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,15 +11,12 @@ int main(int argc, char **argv)
 {
 	int (*op_func)(int, int), a, b;
 
-	if (argc != 4)
-		printf("Error\n"), exit(98);
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
 
-	if (!op_func)
+	if (!op_func || argc != 4)
 		printf("Error\n"), exit(98);
 
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
